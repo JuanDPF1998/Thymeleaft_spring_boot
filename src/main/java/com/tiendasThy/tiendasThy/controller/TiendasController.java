db3f812674d0f933507161636943e50f7f4a08df
JuanDPF <juandanielperezfalcon1998qgmail.com>
2024-11-29 18:52:36 -0600

    crud web finalizado con accion eliminar

--- src/main/java/com/tiendasThy/tiendasThy/controller/TiendasController.java
+++ src/main/java/com/tiendasThy/tiendasThy/controller/TiendasController.java
@@ -3,6 +3,7 @@ package com.tiendasThy.tiendasThy.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,34 +21,35 @@ public class TiendasController {
 	@Autowired
 	private TiendasService service;
 	
-	//METODO ACCION LISTAR TODOS 
+	//ACCION METODO LISTAR
 	@GetMapping("/listar")
 	public String listarTodos(Model model)
 	{
 		model.addAttribute("tiendas", service.listar());
 		return "Listar_Tiendas";
 	}
-	//FORMULARIO AGREGAR NUEVO REGISTRO 
+	//MUESTRA FORMULARIO DE LA VISTA INSERTAR 
 	@GetMapping("/registrar")
 	public String formularioRegistrar(Model model)
 	{
 		model.addAttribute("tienda", new Tiendas());
 		return "Agregar_Tiendas";
 	}
+	//METODO ACCION INSERTAR
 	@PostMapping("/registrarNuevo")
 	public String guardarRegistro(@ModelAttribute Tiendas tienda)
 	{
 		service.agregar(tienda);
 		return "redirect:/api/tiendas/listar";
 	}
-	//FORMULARIO EDITAR / ACTUALIZAR
+	//MUESTRA FORMULARIO REGISTRO POR ID
 	@GetMapping("/editar/{id}")
 	public String formularioEditar(@PathVariable Long id, Model model)
 	{
 		model.addAttribute("tienda", service.obtenerPorId(id));
 		return "Editar_Tiendas";
 	}
-	//ACCION METODO ACTUALIZAR
+	//ACCION METODO ACTUALIZAR POR ID
 	@PostMapping("/actualizar/{id}")
 	public String actualizarRegistro(@PathVariable Long id, @ModelAttribute("tienda") Tiendas tiendas, Model model)
 	{
@@ -59,6 +61,13 @@ public class TiendasController {
 		existe.setTelefono(tiendas.getTelefono());
 		
 		service.agregar(existe);
+		return "redirect:/api/tiendas/listar";
+	}
+	//ACCION METODO ELIMINAR POD ID
+	@GetMapping("/eliminar/{id}")
+	public String eliminarPorId(@PathVariable Long id)
+	{
+		service.eliminarPorId(id);
 		return "redirect:/api/tiendas/listar";
 	}
 }
